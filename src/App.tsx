77fd82fa6258a6d1039656a7ad3f8daf5e3770b0
victorfernandesraton <vfbraton@gmail.com>
2021-03-16 22:20:16 -0300

    initial app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import FavoriteCity from "./components/FavoriteCity";
+import TemperatureTeaser from "./components/TemperarureTeaser";
 
 function App() {
+  const [show, setShow] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FavoriteCity />
+      <button onClick={(e) => setShow(!show)}>Esconder</button>
+      {show && <TemperatureTeaser />}
     </div>
   );
 }
